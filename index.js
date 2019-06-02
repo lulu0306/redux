298473b949289a53c7cee53660f069b7887709e3
lulu0306 <malumel03@gmail.com>
2019-06-02 07:27:09 -0500

    declare the store

---
+++ index.js
@@ -0,0 +1,11 @@
+function createStore(){
+	//The store should have four parts 
+	// 1. The state
+	// 2. Get the state
+	// 3.Listen for changes on the state
+	// 4.update the state
+
+	let state
+
+	const getState = () => state 
+}
